2f7150bba78442b8f751715be6ff0141ff652449
Abu Hurayra <97868096+the5orcerer@users.noreply.github.com>
2025-03-23 03:05:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ init(autoreset=True)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 VERSION_FILE = "version.txt"
-REMOTE_VERSION_URL = "https://raw.githubusercontent.com/the5orcerer/LeakJS/main/version.txt"
+REMOTE_VERSION_URL = "https://raw.githubusercontent.com/the5orcerer/LeakJS/refs/heads/main/version.txt"
 REGEX_REPO = "https://github.com/the5orcerer/Bishop"
 
 def get_local_version() -> str:
